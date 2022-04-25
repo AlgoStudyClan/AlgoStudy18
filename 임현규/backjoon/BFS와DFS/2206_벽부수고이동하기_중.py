15f96f300de620fe0df7fbfbdd1c55fc7d6a7066
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-04-26 08:55:30 +0900

    Error: 벽 부수고 이동하기
    https://www.acmicpc.net/problem/2206
    
    음 문제가 발생했다... 어느 부분일까

---
+++ 임현규/backjoon/BFS와DFS/2206_벽부수고이동하기_중.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+board = [list(input().rstrip()) for _ in range(N)]
+
+
+
+
+def bfs(board):
+    q = deque([(1, 0, 0)])
+    visited = set()
+    visited.add((0, 0))
+    time = 1
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
+    
+    while q:
+        for _ in range(len(q)):
+            talent, y, x = q.popleft()
+            if y == N - 1 and x == M - 1:
+                return time
+            for i in range(4):
+                ny = y + dy[i]
+                nx = x + dx[i]
+                if 0 <= ny < N and 0 <= nx < M and (ny, nx) not in visited:
+                    if board[ny][nx] == '1' and talent == 1:
+                        q.append((talent - 1, ny, nx))
+                        visited.add((ny, nx))
+                    elif board[ny][nx] == '0':
+                        q.append((talent, ny, nx))
+                        visited.add((ny, nx))
+        time += 1
+    return -1
+
+
+result = bfs(board)
+print(result)
